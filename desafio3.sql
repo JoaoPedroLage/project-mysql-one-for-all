fcda7b754a35074cf312ae5d6db880a5045a825e
JoaoPedroLage <JoaoPedro.Lage@outlook.com>
2022-02-07 11:07:23 -0300

    feat: finish desafio3.sql

--- desafio3.sql
+++ desafio3.sql
@@ -1,12 +1,12 @@
-SELECT User.user_name AS usuario, COUNT(Rep.user_id) AS qtde_musicas_ouvidas
-FROM SpotifyClone.users AS User
-INNER JOIN SpotifyClone.reproductions AS Rep
-ON Rep.user_id = User.id
-GROUP BY User.user_name
-ORDER BY User.user_name;
-
-SELECT Song.id, Song.duration_sec/60 AS total_minutos
-FROM SpotifyClone.songs AS Song
-INNER JOIN SpotifyClone.reproductions AS Rep
-ON Song.id = Rep.song_id
-GROUP BY Song.id;
+SELECT 
+    User.user_name AS usuario,
+    COUNT(Rep.song_id) AS qtde_musicas_ouvidas,
+    ROUND((SUM(Song.duration_sec) / 60), 2) AS total_minutos
+FROM
+    SpotifyClone.reproductions AS Rep
+        JOIN
+    SpotifyClone.users AS User ON User.id = Rep.user_id
+        JOIN
+    SpotifyClone.songs AS Song ON Song.id = Rep.song_id
+GROUP BY usuario
+ORDER BY usuario;
