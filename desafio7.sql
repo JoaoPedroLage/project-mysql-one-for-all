f6ef7d3092d15d4035eaa504379945e634c4770d
JoaoPedroLage <JoaoPedro.Lage@outlook.com>
2022-02-07 12:49:32 -0300

    feat: finish desafio7.sql

---
+++ desafio7.sql
@@ -0,0 +1,12 @@
+SELECT 
+    Artist.artist_name AS artista,
+    Album.album_name AS album,
+    COUNT(Follow.following_artist_id) AS seguidores
+FROM
+    SpotifyClone.albums AS Album
+        JOIN
+    SpotifyClone.followers AS Follow ON Album.artist_id = Follow.following_artist_id
+		JOIN
+	SpotifyClone.artists AS Artist ON Album.artist_id = Artist.id
+    GROUP BY artista, album
+    ORDER BY seguidores DESC, artista, album;
