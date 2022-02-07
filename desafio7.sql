38e5f4458e3dfab22255473ae8356046d4ad10b7
JoaoPedroLage <JoaoPedro.Lage@outlook.com>
2022-02-07 12:51:36 -0300

    refactor: req 7

--- desafio7.sql
+++ desafio7.sql
@@ -6,7 +6,7 @@ FROM
     SpotifyClone.albums AS Album
         JOIN
     SpotifyClone.followers AS Follow ON Album.artist_id = Follow.following_artist_id
-		JOIN
-	SpotifyClone.artists AS Artist ON Album.artist_id = Artist.id
-    GROUP BY artista, album
-    ORDER BY seguidores DESC, artista, album;
+        JOIN
+    SpotifyClone.artists AS Artist ON Album.artist_id = Artist.id
+GROUP BY artista , album
+ORDER BY seguidores DESC , artista , album;
