c61ee0f3e1ebf1b4144927c36efc5fc2678618d8
JoaoPedroLage <JoaoPedro.Lage@outlook.com>
2022-02-07 12:25:23 -0300

    refactor: reqs 2

--- desafio2.sql
+++ desafio2.sql
@@ -1,4 +1,8 @@
-SELECT MAX(Song.id) AS cancoes, MAX(Song.artist_id) AS artistas, MAX(Song2.album_id) AS albuns
-FROM SpotifyClone.songs AS Song
-JOIN SpotifyClone.songs AS Song2
-ON Song.album_id = Song2.album_id;
+SELECT 
+    MAX(Song.id) AS cancoes,
+    MAX(Song.artist_id) AS artistas,
+    MAX(Song2.album_id) AS albuns
+FROM
+    SpotifyClone.songs AS Song
+        JOIN
+    SpotifyClone.songs AS Song2 ON Song.album_id = Song2.album_id;
